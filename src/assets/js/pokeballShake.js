7c954cdc313d8ea6075dfabad271f7aa665a7228
Robert <robstow94@gmail.com>
2019-07-10 00:27:52 +0100

    WIP

--- src/assets/js/pokeballShake.js
+++ src/assets/js/pokeballShake.js
@@ -4,17 +4,11 @@ animated.forEach(animate => {
   
   const animatedSpan = animate.querySelector('.animatedSpan');
   
-  animate.addEventListener('mouseover', () => {
-    animatedSpan.classList.add('mouseOver');
-  });
+  animate.addEventListener('mouseover', () => animatedSpan.classList.add('mouseOver'));
   
-  animate.addEventListener('mouseout', () => {
-    animatedSpan.classList.remove('mouseOver');
-  });
+  animate.addEventListener('mouseout', () => animatedSpan.classList.remove('mouseOver'));
 
-  animate.addEventListener('animationend', () => {
-    animate.classList.toggle('shake');
-  });
+  animate.addEventListener('animationend', () => animate.classList.toggle('shake'));
 
 });
 
